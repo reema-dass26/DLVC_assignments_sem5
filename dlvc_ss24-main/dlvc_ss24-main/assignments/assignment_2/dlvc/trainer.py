6dc27d1898d171cd42d2d930fbf959d55bf86981
reema-dass26 <reemaworkmail@gmail.com>
2024-05-15 16:08:06 +0200

    trainer..py implementation

--- dlvc_ss24-main/dlvc_ss24-main/assignments/assignment_2/dlvc/trainer.py
+++ dlvc_ss24-main/dlvc_ss24-main/assignments/assignment_2/dlvc/trainer.py
@@ -1,11 +1,12 @@
 import collections
 import torch
-from typing import  Tuple
+from typing import Tuple
 from abc import ABCMeta, abstractmethod
 from pathlib import Path
 from tqdm import tqdm
 
-#from dlvc.wandb_logger import WandBLogger
+from dlvc.wandb_logger import WandBLogger
+from dlvc.dataset.oxfordpets import OxfordPetsCustom
 
 class BaseTrainer(metaclass=ABCMeta):
     '''
@@ -78,13 +79,34 @@ class ImgSemSegTrainer(BaseTrainer):
 
         '''
         
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.lr_scheduler = lr_scheduler
 
-    
-        ##TODO implement
-        # recycle your code from assignment 1 or use/adapt reference implementation
-        pass
+        self.device = device
+
+        self.num_epochs = num_epochs
+        self.train_metric = train_metric
+        self.val_metric = val_metric
+
+        self.subtract_one = isinstance(train_data, OxfordPetsCustom)
         
+        self.train_data_loader = torch.utils.data.DataLoader(train_data,
+                                          batch_size=batch_size,
+                                          shuffle=True,
+                                          num_workers=2)
+    
+        self.val_data_loader = torch.utils.data.DataLoader(val_data,
+                                          batch_size=batch_size,
+                                          shuffle=False,
+                                          num_workers=1)
+        self.num_train_data = len(train_data)
+        self.num_val_data = len(val_data)
 
+        self.checkpoint_dir = training_save_dir
+        self.wandb_logger = WandBLogger(enabled=True, model=model, run_name=model.net._get_name())
+        
     def _train_epoch(self, epoch_idx: int) -> Tuple[float, float]:
         """
         Training logic for one epoch. 
@@ -93,12 +115,46 @@ class ImgSemSegTrainer(BaseTrainer):
 
         epoch_idx (int): Current epoch number
         """
-        ##TODO implement
-        # recycle your code from assignment 1 or use/adapt reference implementation
-        pass
+        self.model.train()
+        epoch_loss = 0.
+        self.train_metric.reset()
+        
+        # Train epoch
+        for i, batch in tqdm(enumerate(self.train_data_loader), desc="train", total=len(self.train_data_loader)):
+            self.optimizer.zero_grad()
 
+            # Get the inputs; data is a list of [inputs, labels]
+            inputs, labels = batch
+            labels = labels.squeeze(1) - 1 if self.subtract_one else labels.squeeze(1)
+            batch_size = inputs.shape[0]  # b x ..?
 
-    def _val_epoch(self, epoch_idx:int) -> Tuple[float, float]:
+            # Make predictions for this batch
+            outputs = self.model(inputs.to(self.device))
+            if isinstance(outputs, collections.OrderedDict):
+                outputs = outputs['out']
+
+            # Compute the loss and its gradients
+            loss = self.loss_fn(outputs, labels.to(self.device))
+            loss.backward()
+
+            # Adjust learning weights
+            self.optimizer.step()
+
+            # Gather metrics
+            epoch_loss += (loss.item() * batch_size)
+            self.train_metric.update(outputs.detach().cpu(), labels.detach().cpu())
+      
+        self.lr_scheduler.step()
+        epoch_loss /= self.num_train_data
+        epoch_mIoU = self.train_metric.mIoU()
+        
+        print(f"______epoch {epoch_idx} \n")
+        print(f"Loss: {epoch_loss}")
+        print(self.train_metric)
+
+        return epoch_loss, epoch_mIoU
+
+    def _val_epoch(self, epoch_idx: int) -> Tuple[float, float]:
         """
         Validation logic for one epoch. 
         Prints current metrics at end of epoch.
@@ -106,9 +162,37 @@ class ImgSemSegTrainer(BaseTrainer):
 
         epoch_idx (int): Current epoch number
         """
-        ##TODO implement
-        # recycle your code from assignment 1 or use/adapt reference implementation
-        pass
+        self.val_metric.reset()
+        epoch_loss = 0.
+
+        # Validation epoch
+        for batch_idx, batch in tqdm(enumerate(self.val_data_loader), desc="eval", total=len(self.val_data_loader)):
+            self.model.eval()
+            with torch.no_grad():
+                # Get the inputs; data is a tuple of [inputs, labels]
+                inputs, labels = batch
+                labels = labels.squeeze(1) - 1 if self.subtract_one else labels.squeeze(1)
+                batch_size = inputs.shape[0] 
+
+                # Make predictions for this batch
+                outputs = self.model(inputs.to(self.device))
+                if isinstance(outputs, collections.OrderedDict):
+                    outputs = outputs['out']
+
+                # Compute the loss and its gradients
+                loss = self.loss_fn(outputs, labels.to(self.device))
+
+                # Gather metrics
+                epoch_loss += (loss.item() * batch_size)
+                self.val_metric.update(outputs.cpu(), labels.cpu())
+
+        epoch_loss /= self.num_val_data
+        epoch_mIoU = self.val_metric.mIoU()
+        print(f"______epoch {epoch_idx} - validation \n")
+        print(f"Loss: {epoch_loss}")
+        print(self.val_metric)
+
+        return epoch_loss, epoch_mIoU
 
     def train(self) -> None:
         """
@@ -118,17 +202,31 @@ class ImgSemSegTrainer(BaseTrainer):
         than currently saved best mean IoU or if it is end of training. 
         Depending on the val_frequency parameter, validation is not performed every epoch.
         """
-        ##TODO implement
-        # recycle your code from assignment 1 or use/adapt reference implementation
-        pass
+        best_mIoU = 0.
+        for epoch_idx in range(self.num_epochs):
 
-                
+            train_loss, train_mIoU = self._train_epoch(epoch_idx)
 
+            wandb_log = {'epoch': epoch_idx}
 
+            # log stuff
+            wandb_log.update({"train/loss": train_loss})
+            wandb_log.update({"train/mIoU": train_mIoU})
 
+            if epoch_idx % self.val_frequency == 0:
+                val_loss, val_mIoU = self._val_epoch(epoch_idx)
+                wandb_log.update({"val/loss": val_loss})
+                wandb_log.update({"val/mIoU": val_mIoU})
 
+                if best_mIoU <= val_mIoU:
+                    print(f"####best mIou: {val_mIoU}")
+                    print(f"####saving model to {self.checkpoint_dir}")
+                    self.model.save(Path(self.checkpoint_dir), suffix="best")
+                    best_mIoU = val_mIoU
+                if epoch_idx == self.num_epochs-1:
+                    self.model.save(Path(self.checkpoint_dir), suffix="last")
 
-            
-            
-
-
+            self.wandb_logger.log(wandb_log)
+  
+    def dispose(self) -> None:
+        self.wandb_logger.finish()
