96a3f11c7e2e07fb3e4de468fafa2f025a1d895f
reema-dass26 <reemaworkmail@gmail.com>
2024-05-24 16:05:25 +0200

    part 3

--- dlvc_ss24-main/dlvc_ss24-main/assignments/assignment_2/train.py
+++ dlvc_ss24-main/dlvc_ss24-main/assignments/assignment_2/train.py
@@ -2,10 +2,12 @@
 import argparse
 import os
 import torch
+from torchvision.models.segmentation import FCN
+from torchvision.models.segmentation.fcn import FCNHead
 import torchvision.transforms.v2 as v2
+import torch.optim as optim
 from pathlib import Path
 from torchvision.models.segmentation import fcn_resnet50
-
 from dlvc.models.segment_model import DeepSegmenter
 from dlvc.dataset.oxfordpets import  OxfordPetsCustom
 from dlvc.metrics import SegMetrics
@@ -15,20 +17,17 @@ import os
 import torch
 from pathlib import Path
 import os
-from datetime import datetime
+import torch.nn as nn
 
-# from dlvc.models.class_model import DeepClassifier  # etc. change to your model
-# from dlvc.models.cnn import YourCNN
-# from dlvc.models.vit import ViT
+from datetime import datetime
 from dlvc.metrics import Accuracy
 from dlvc.trainer import ImgClassificationTrainer
-# from dlvc.datasets.cifar10 import CIFAR10Dataset
-# from dlvc.datasets.dataset import Subset
-
-
-# from torchvision.models import resnet18
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import ExponentialLR, LinearLR, StepLR
+
+from pathlib import Path
+
+p = Path('dlvc_ss24-main\\dlvc_ss24-main\\assignments\\assignment_2\\data')
 
 def train(args):
 
@@ -48,14 +47,14 @@ def train(args):
                             v2.ToDtype(torch.long, scale=False),
                             v2.Resize(size=(64,64), interpolation=v2.InterpolationMode.NEAREST)])
 
-    train_data = OxfordPetsCustom(root="path_to_dataset", 
+    train_data = OxfordPetsCustom(root=p, 
                             split="trainval",
                             target_types='segmentation', 
                             transform=train_transform,
                             target_transform=train_transform2,
                             download=True)
 
-    val_data = OxfordPetsCustom(root="path_to_dataset", 
+    val_data = OxfordPetsCustom(root=p, 
                             split="test",
                             target_types='segmentation', 
                             transform=val_transform,
@@ -75,35 +74,86 @@ def train(args):
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
-    models = []
-    if args.model == "fcn_resnet50":
-        models.append(DeepSegmenter(fcn_resnet50()))
-    # elif args.model == "cnn":
-    #     models.append(YourCNN())
-    # elif args.model == "vit":
-    #     models.append(ViT())
-    elif args.model =='deeplabv3_resnet50':
-        # models.append(DeepSegmenter(deeplabv3_resnet50())) #TODO
-        # models.append(YourCNN())
-        # models.append(ViT())
+    
+    if args.model == "fcn_resnet50": #Pretrained model selection for the assignment
+        if args.pretrained=='False':
+            # models.append(DeepSegmenter(fcn_resnet50(pretrained=False)))
+            model= DeepSegmenter(fcn_resnet50(pretrained=False))
+
+            optimizer = optim.Adam(model.parameters(), lr=0.001)
+            loss_fn = nn.CrossEntropyLoss()
+            
+            train_metric = SegMetrics(classes=train_data.classes_seg)
+            val_metric = SegMetrics(classes=val_data.classes_seg)
+            val_frequency = 2
+
+            model_save_dir = Path("saved_models")
+            model_save_dir.mkdir(exist_ok=True)
+
+            lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1) 
+            
+            trainer = ImgSemSegTrainer(model, 
+                            optimizer,
+                            loss_fn,
+                            lr_scheduler,
+                            train_metric,
+                            val_metric,
+                            train_data,
+                            val_data,
+                            device,
+                            args.num_epochs, 
+                            model_save_dir,
+                            batch_size=64,
+                            val_frequency = val_frequency)
+            trainer.train()
+
+
+            trainer.dispose() 
+
+
+        else:
+            # models.append(DeepSegmenter(fcn_resnet50(pretrained=True)))
+            model=DeepSegmenter(fcn_resnet50(pretrained=True))
+            model.eval()  
+            val_data_loader = torch.utils.data.DataLoader(val_data,
+                                          batch_size=64,
+                                          shuffle=False,
+                                          num_workers=1)
+            num_train_data = len(train_data)
+            num_val_data = len(val_data)
+            with torch.no_grad():  # Disable gradient calculation during validation
+                # Iterate over the validation dataset
+                for images, targets in val_data_loader:
+                    images, targets = images.to(device), targets.to(device)
+
+                    # Perform inference
+                    outputs = model(images)['out']
+
+                    # Evaluate the predictions (you can use appropriate metrics here)
+                    # For example, you can compute accuracy, IoU, etc.
+                    # You need to implement this part based on your specific requirements
+
+    
+            
+
+    # elif args.model =='deeplabv3_resnet50':
+  
+    #     pass
 
     # model = DeepSegmenter(...)
-    for model in models:
-        print(f"==> Started {model.mname()}")
-        model_save_dir = model_save_dir / model.mname()
-        model.to(device)
+    # for model in models:
+    #     print(f"==> Started {model.mname()}")
+    #     model_save_dir = model_save_dir / model.mname()
+    #     model.to(device)
         
-        optimizer = AdamW(
-            model.parameters(), 
-            weight_decay=args.weight_decay,
-            lr=args.learning_rate, 
-            amsgrad=args.amsgrad,
-        )
-        loss_fn = torch.nn.CrossEntropyLoss()
+    #     optimizer = AdamW(
+    #         model.parameters(), 
+    #         weight_decay=args.weight_decay,
+    #         lr=args.learning_rate, 
+    #         amsgrad=args.amsgrad,
+    #     )
+    #     loss_fn = torch.nn.CrossEntropyLoss()
 
-        train_metric = Accuracy(classes=train_data.classes)
-        val_metric = Accuracy(classes=val_data.classes)
-        val_frequency = 5
 
     optimizer = ...
     loss_fn = ...
@@ -140,6 +190,12 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser(description='Training')
     args.add_argument('-d', '--gpu_id', default='0', type=str,
                       help='index of which GPU to use')
+    args.add_argument(
+        "-p", "--pretrained", default="False", choices=["True", "False"]
+    )
+    args.add_argument(
+        "-m", "--model", default="resnet", choices=["fcn_resnet50", ]
+    )
     
     if not isinstance(args, tuple):
         args = args.parse_args()
